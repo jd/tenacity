bac9ab8621d54428cdb5f9778acbb121f2e2fa11
Ray Holder <ray@blacklocus.com>
2014-05-05 12:30:39 -0500

    bump version up to 1.2.2-dev

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ CLASSIFIERS = [
 
 settings.update(
     name='retrying',
-    version='1.2.1',
+    version='1.2.2-dev',
     description='Retrying',
     long_description=open('README.rst').read() + '\n\n' +
                      open('HISTORY.rst').read(),
