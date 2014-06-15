868fcd8ede2d72fa5afb0207efbdfb430edeff07
Simon Dollé <simon.dolle@gmail.com>
2014-06-15 16:27:54 +0200

    Test exception type on failure.
    When we stop retrying, because of recurring exception
    we expect the decorator to raise the last raised exception
    without any wrapping (if wrap_exception==False)
    cf #8
    We test this behavior in a new unit test.

--- test_retrying.py
+++ test_retrying.py
@@ -282,7 +282,7 @@ class TestDecoratorWrapper(unittest.TestCase):
         self.assertTrue(t >= 250)
         self.assertTrue(result)
 
-    def test_with_stop(self):
+    def test_with_stop_on_return_value(self):
         try:
             _retryable_test_with_stop(NoneReturnUntilAfterCount(5))
             self.fail("Expected RetryError after 3 attempts")
@@ -290,6 +290,14 @@ class TestDecoratorWrapper(unittest.TestCase):
             self.assertFalse(re.last_attempt.has_exception)
             self.assertEqual(3, re.last_attempt.attempt_number)
             self.assertTrue(re.last_attempt.value is None)
+            print(re)
+
+    def test_with_stop_on_exception(self):
+        try:
+            _retryable_test_with_stop(NoIOErrorAfterCount(5))
+            self.fail("Expected IOError")
+        except IOError as re:
+            self.assertTrue(isinstance(re, IOError))
             print(re)
 
     def test_retry_if_exception_of_type(self):
