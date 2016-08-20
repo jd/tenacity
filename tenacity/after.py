5c30f37ca3c8f061f6a54fcad7d96671b11b807c
Joshua Harlow <harlowja@gmail.com>
2016-08-19 21:24:33 -0700

    Add before/after strategies
    Make these before and after functions more official
    by providing some basic ones and a no-op ones that
    will be used by default.

---
+++ tenacity/after.py
@@ -0,0 +1,34 @@
+# Copyright 2016 Julien Danjou
+# Copyright 2016 Joshua Harlow
+# Copyright 2013-2014 Ray Holder
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from tenacity import _utils
+
+
+def after_nothing(func, trial_number, trial_time_taken_ms):
+    """After call strategy that does nothing."""
+
+
+def after_log(logger, log_level):
+    """After call strategy that logs to some logger the finished attempt."""
+
+    def log_it(func, trial_number, trial_time_taken_ms):
+        logger.log(log_level,
+                   "Finished call to '%s' after %i(ms), "
+                   "this was the %i time calling it.",
+                   _utils.get_callback_name(func), trial_time_taken_ms,
+                   trial_number)
+
+    return log_it
