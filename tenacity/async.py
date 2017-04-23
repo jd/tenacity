292f1ca5bd4b3f59f0ec722eb96aaba7f97a123b
Heri Sim <heri16@gmail.com>
2017-04-24 00:29:38 +0800

    Add Context Manager to AsyncRetrying class

--- tenacity/async.py
+++ tenacity/async.py
@@ -18,6 +18,7 @@
 
 import asyncio
 import sys
+import six
 
 from tenacity import BaseRetrying
 from tenacity import DoAttempt
@@ -26,6 +27,12 @@ from tenacity import NO_RESULT
 
 
 class AsyncRetrying(BaseRetrying):
+    def __init__(self,
+                 fn=None,
+                 **kwargs):
+        super(Retrying, self).__init__(**kwargs)
+        self.fn = fn if asyncio.iscoroutinefunction(fn) else asyncio.coroutine(fn)
+
     @asyncio.coroutine
     def call(self, fn, *args, **kwargs):
         self.begin(fn)
@@ -50,3 +57,25 @@ class AsyncRetrying(BaseRetrying):
                 yield from asyncio.sleep(do)
             else:
                 return do
+
+    def context(self, fn):
+        self.fn = fn if asyncio.iscoroutinefunction(fn) else asyncio.coroutine(fn)
+
+    def __enter__(self):
+        # Note: A synchronous context manager is NOT able to suspend execution in its enter and exit methods.
+        if not self.fn:
+            return
+        
+        r = self
+        f = self.fn
+
+        @six.wraps(f)
+        def wrapped_f(*args, **kw):
+          return r.call(f, *args, **kw)
+
+        wrapped_f.retry = r
+        return wrapped_f
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # Note: If we returned True here, any exception inside the with block would be suppressed!
+        return
