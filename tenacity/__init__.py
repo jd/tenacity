9c8a6d97f58653a8f5ad09845f021ad5ebbd56bc
Joshua Harlow <jxharlow@godaddy.com>
2017-11-28 12:55:55 -0800

    Missed top level import of stop_when_event_set
    Don't make me import from stop module itself ;)

--- tenacity/__init__.py
+++ tenacity/__init__.py
@@ -58,6 +58,7 @@ from .stop import stop_after_delay  # noqa
 from .stop import stop_all  # noqa
 from .stop import stop_any  # noqa
 from .stop import stop_never  # noqa
+from .stop import stop_when_event_set  # noqa
 
 # Import all built-in wait strategies for easier usage.
 from .wait import wait_chain  # noqa
