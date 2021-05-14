cbd214e653ad2ba2bb7c9dfde3e4449a276d687b
akgnah <1024@setq.me>
2021-05-14 11:27:37 +0800

    Fix after_log logger format

--- tenacity/after.py
+++ tenacity/after.py
@@ -25,12 +25,11 @@ def after_log(logger, log_level, sec_format="%0.3f"):
     """After call strategy that logs to some logger the finished attempt."""
 
     def log_it(retry_state):
-        sec = sec_format % _utils.get_callback_name(retry_state.fn)
         logger.log(
             log_level,
             "Finished call to '{0}' after {1}(s), this was the {2} time calling it.".format(
-                sec,
-                retry_state.seconds_since_start,
+                _utils.get_callback_name(retry_state.fn),
+                sec_format % retry_state.seconds_since_start,
                 _utils.to_ordinal(retry_state.attempt_number),
             ),
         )
