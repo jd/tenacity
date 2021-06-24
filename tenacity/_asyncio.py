52ce97c61bf7f661a7e9d7f32896151238f202b3
Alexey Stepanov <penguinolog@users.noreply.github.com>
2021-06-24 14:05:10 +0200

    Fix issue #288 : __name__ and other attributes for async functions (#312)
    `iscoroutinefunction` is always not Nons -> remove check

--- tenacity/_asyncio.py
+++ tenacity/_asyncio.py
@@ -14,6 +14,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import functools
 import sys
 from asyncio import sleep
 
@@ -70,6 +72,7 @@ class AsyncRetrying(BaseRetrying):
         fn = super().wraps(fn)
         # Ensure wrapper is recognized as a coroutine function.
 
+        @functools.wraps(fn)
         async def async_wrapped(*args, **kwargs):
             return await fn(*args, **kwargs)
 
